d404e2eadcbb4f379e3d405edb5f321a682dded8
Paulovich Anton <p.a.n.tosha@gmail.com>
2014-12-04 00:56:34 +0300

    Пример работы встроенного решателя лин.программирования.

---
+++ Lab1/builtinsimplex.m
@@ -0,0 +1,7 @@
+c = [1; -3; -5; -1];
+b = [5; 9];
+A = [1, 4, 4, 1;
+    1, 7, 8, 2];
+x = [1; 0; 1; 0];
+options = optimset('Algorithm', 'simplex');
+solve = linprog(-c, [], [], A, b, zeros(4,1), [], [], options);
